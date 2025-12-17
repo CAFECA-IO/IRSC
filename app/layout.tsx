1bc691e17d6856e00c42b002040305311619dbbc
Luphia Chang <luphia@mermer.cc>
2025-12-17 15:27:35 +0800

    fix global.css path

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import '@/globals.css'
+import '@/app/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 const url = process.env.NEXT_PUBLIC_APP_URL || 'https://irsc.isunfa.com'
